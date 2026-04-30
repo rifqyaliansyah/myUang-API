62a8e15628c6a7233f5b42a937126a32c685ac6b
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-04-30 19:49:50 +0700

    add validation messages for login email and password fields

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -18,8 +18,8 @@ router.post('/register',
 
 router.post('/login',
     [
-        body('email').isEmail().normalizeEmail(),
-        body('password').notEmpty(),
+        body('email').isEmail().normalizeEmail().withMessage('Valid email required'),
+        body('password').notEmpty().withMessage('Password is required'),
     ],
     validate,
     controller.login
